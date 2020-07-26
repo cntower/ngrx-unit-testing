a484975e02e292d996ff3082a2f3e1548f9069cf
cntower <stasbaturin78@gmail.com>
2020-07-26 12:47:42 +0200

    testing effects

--- src/app/app-store/pattern/pattern.actions.spec.ts
+++ src/app/app-store/pattern/pattern.actions.spec.ts
@@ -1,15 +1,15 @@
 import * as Actions from './pattern.actions';
 
 fdescribe('Store > Pattern > PatternActions', () => {
-  it('SHOULD create a loadPatterns action', () => {
+  it('should create a loadPatterns action', () => {
     const action = Actions.loadPatterns;
     expect(action.type).toEqual('[Pattern/UI] Load Patterns');
   });
-  it('SHOULD create a loadPatternsSuccess action', () => {
+  it('should create a loadPatternsSuccess action', () => {
     const action = Actions.loadPatternsSuccess({ patterns: [] });
     expect(action.type).toEqual('[Pattern/API] Load Patterns Success');
   });
-  it('SHOULD create a loadPatternsFailure action', () => {
+  it('should create a loadPatternsFailure action', () => {
     const action = Actions.loadPatternsFailure({ error: {} });
     expect(action.type).toEqual('[Pattern/API] Load Patterns Failure');
   });
--- src/app/app-store/pattern/pattern.effects.spec.ts
+++ src/app/app-store/pattern/pattern.effects.spec.ts
@@ -1,25 +1,67 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 
 import { PatternEffects } from './pattern.effects';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PatternService } from 'src/app/patterns/pattern.service';
+import { Pattern } from './pattern.model';
+
+const PATTERNS: Pattern[] = [
+  {
+    id: 1,
+    name: 'Abstract factory',
+    description: 'factory groups object factories that have a common theme.',
+    type: 'Creational',
+  },
+];
 
 fdescribe('PatternEffects', () => {
-  let actions$: Observable<any> = of({ type: '[Pattern/UI] Load Patterns' });
+  let actions$: Observable<any>;
   let effects: PatternEffects;
-
+  let patternService: PatternService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [PatternEffects, provideMockActions(() => actions$)],
+      providers: [
+        PatternEffects,
+        PatternService,
+        provideMockActions(() => actions$),
+      ],
     });
+
+    patternService = TestBed.inject(PatternService);
+    patternService.getPatterns = jasmine
+      .createSpy()
+      .and.returnValue(of(PATTERNS));
 
     effects = TestBed.inject(PatternEffects);
   });
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+  it('should get action "[Pattern/API] Load Patterns Success"', () => {
+    actions$ = of({ type: '[Pattern/UI] Load Patterns' });
+    effects.loadPatterns$.subscribe((action) => {
+      expect(action).toEqual({
+        type: '[Pattern/API] Load Patterns Success',
+        patterns: PATTERNS,
+      });
+    });
+  });
+
+  it('should get action "[Pattern/API] Load Patterns Failure"', () => {
+    actions$ = of({ type: '[Pattern/UI] Load Patterns' });
+    patternService.getPatterns = jasmine
+      .createSpy()
+      .and.returnValue(throwError({ message: 'Test error' }));
+    effects.loadPatterns$.subscribe((action) => {
+      expect(action).toEqual({
+        type: '[Pattern/API] Load Patterns Failure',
+        error: { message: 'Test error' },
+      });
+    });
   });
 });
